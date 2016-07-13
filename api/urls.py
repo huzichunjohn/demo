f97fa79c3f7debd447c8a793371858735425bb2d
johnhu <huzichunjohn@126.com>
2016-07-13 10:12:02 +0000

    add blog api.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from api import views
 from rest_framework import routers
+from rest_framework.authtoken import views as token_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -8,6 +9,9 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^api-token-auth', token_views.obtain_auth_token),
     url(r'^example/$', views.ExampleView.as_view(), name='example'),
+    url(r'^blog/$', views.BlogList.as_view(), name='blog'),
+    url(r'^blog/(?P<pk>[0-9]+)/$', views.BlogDetail.as_view(), name='blog-detail'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from api.serializers import UserSerializer, GroupSerializer
+from api.serializers import UserSerializer, GroupSerializer, BlogSerializer
+from blog.models import Blog
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -24,3 +26,41 @@ class ExampleView(APIView):
 	    'auth': unicode(request.auth),
 	}
 	return Response(content)
+
+class BlogList(APIView):
+    def get(self, request, format=None):
+	blogs = Blog.objects.all()
+	serializer = BlogSerializer(blogs, many=True, context={'request': request})
+	return Response(serializer.data)
+
+    def post(self, request, format=None):
+	serializer = BlogSerializer(data=request.data, context={'request': request})
+	if serializer.is_valid():
+	    serializer.save()
+	    return Response(serializer.data)
+	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BlogDetail(APIView):
+    def get_object(self, pk):
+	try:
+	    return Blog.objects.get(pk=pk)
+	except Blog.DoesNotExist:
+	    raise Http404
+
+    def get(self, request, pk, format=None):
+	blog = self.get_object(pk)
+	serializer = BlogSerializer(blog, context={'request': request})
+	return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+	blog = self.get_object(pk)
+	serializer = BlogSerializer(blog, data=request.data, context={'request': request})
+	if serializer.is_valid():
+	    serializer.save()
+	    return Response(serializer.data)
+	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	
+    def delete(self, request, pk, format=None):
+	blog = self.get_object(pk)
+	blog.delete()
+	return Response(status=status.HTTP_204_NO_CONTENT) 
