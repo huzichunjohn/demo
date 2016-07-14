8a87aa9ad6c9b4a554f63e1e9b5951351ad6ac51
johnhu <huzichunjohn@126.com>
2016-07-14 02:31:03 +0000

    use generic class based views.

--- api/views.py
+++ api/views.py
@@ -27,30 +27,10 @@ class ExampleView(APIView):
 	}
 	return Response(content)
 
-class BlogList(mixins.ListModelMixin,
-	       mixins.CreateModelMixin,
-	       generics.GenericAPIView):
+class BlogList(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
-    def get(self, request, *args, **kwargs):
-	return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-	return self.create(request, *args, **kwargs)
-
-class BlogDetail(mixins.RetrieveModelMixin,
-	         mixins.UpdateModelMixin,
-	         mixins.DestroyModelMixin,
-	         generics.GenericAPIView):
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-
-    def get(self, request, *args, **kwargs):
-	return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-	return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-	return self.destroy(request, *args, **kwargs)
