7ae17e6c4cba75b062b3889d6c8419ab1e873594
johnhu <huzichunjohn@126.com>
2016-08-15 10:44:49 +0000

    add django-redis.

--- demo/settings.py
+++ demo/settings.py
@@ -173,9 +173,22 @@ CRONJOBS = [
 
 SITE_ID = 1
 
+#CACHES = {
+#    'default': {
+#	'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#	'LOCATION': '127.0.0.1:11211',
+#    }
+#}
+
 CACHES = {
     'default': {
-	'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-	'LOCATION': '127.0.0.1:11211',
+       'BACKEND': 'django_redis.cache.RedisCache',
+       'LOCATION': 'redis://127.0.0.1:6379/1',
+       'OPTIONS': {
+           'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+       },
     }
 }
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
