836e7d015d780c6439d89d2d048e71348529e231
johnhu <huzichunjohn@126.com>
2016-09-05 18:10:42 +0800

    update the user api.

--- api/serializers.py
+++ api/serializers.py
@@ -4,11 +4,30 @@ from blog.models import Blog
 from todo.models import Todo
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    blogs = serializers.PrimaryKeyRelatedField(many=True, queryset=Blog.objects.all())
+    blogs = serializers.PrimaryKeyRelatedField(many=True, queryset=Blog.objects.all(), required=False)
 
     class Meta:
 	model = User
-	fields = ('url', 'username', 'email', 'groups', 'blogs')
+	fields = ('id', 'url', 'username', 'password', 'email', 'groups', 'blogs', 'is_staff', 'is_superuser', 'is_active', 'date_joined')
+        read_only_fields = ('id', 'is_staff', 'is_superusesr', 'is_active', 'date_joined')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(username=validated_data['username'], email=validated_data['email'])
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- demo/settings.py
+++ demo/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = (
     'rest_auth.registration',
     'django_extensions',
     'corsheaders',
+    'logentry_admin',
 )
 
 MIDDLEWARE_CLASSES = (
